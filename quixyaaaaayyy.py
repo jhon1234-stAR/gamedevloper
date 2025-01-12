ef4acc3aa32aaf937820a4737adbcf6cbd40fea7
jhon1234-stAR <rakrobinson@gmail.com>
2025-01-12 18:00:18 +0000

    Add files via upload

--- quixyaaaaayyy.py
+++ quixyaaaaayyy.py
@@ -5,6 +5,7 @@ TITLE="BOB"
 score=0
 totaltime=30
 WIDTH=871
+
 HEIGHT=651
 qfile="questions.txt"
 gameover=False
@@ -12,6 +13,7 @@ win=False
 q=[]
 qcount=0
 qinex=0
+
 scrollybox=Rect(1,1,872,81)
 boxy=Rect(21,100,600,151)
 ans1=Rect(21,270,300,151)
@@ -20,7 +22,7 @@ ans3=Rect(20,450,300,151)
 ans4=Rect(370,450,300,151)
 timey=Rect(700,100,121,151)
 skipy=Rect((700,270,121,330))
-
+aboxys=[ans1,ans2,ans3,ans4]
 def draw():
     screen.clear()
     screen.fill("black")
@@ -34,9 +36,14 @@ def draw():
     screen.draw.filled_rect(skipy,"yellow")
 
     txt=f"welcome to the quiz!!question number:{qinex} of {qcount}  "
-    screen.draw.textbox(txt,scrollybox,color="black")
-    screen.draw.textbox(str(totaltime),timey,color="black")
-    screen.draw.textbox("skip",skipy,color="black",angle=-90)
+    screen.draw.textbox(txt,scrollybox,color="black",shadow=(0.5,0.5),scolor="red")
+    screen.draw.textbox(str(totaltime),timey,color="black",shadow=(0.5,0.5),scolor="red")
+    screen.draw.textbox("skip",skipy,color="black",angle=-90,shadow=(0.5,0.5),scolor="red")
+    screen.draw.textbox(question[0],boxy,color="black",shadow=(0.5,0.5),scolor="red")
+    hi=1
+    for i in aboxys:
+        screen.draw.textbox(question[hi],i,color="black",shadow=(0.5,0.5),scolor="red")
+        hi+=1
 
 def readquestions():
     global qcount
@@ -51,7 +58,16 @@ def update():
         scrollybox.x=871
 
 readquestions()
-print(q)
+def nextquestions():
+    global qinex
+
+    qinex+=1
+    return q.pop(0).split("|")
+
+
+
+question=nextquestions()
+
 pgzrun.go()
 
 
