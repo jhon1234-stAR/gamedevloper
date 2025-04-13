fee1c10493cdf243d63d589ea58162088ee66875
jhon1234-stAR <rakrobinson@gmail.com>
2025-04-13 17:23:43 +0100

    Add files via upload

--- destroy_block.py
+++ destroy_block.py
@@ -17,8 +17,8 @@ class bob(pygame.sprite.Sprite):
     def update(self):
         self.rect.y+=1
 
-        if self.rect.bottom<800:
-            self.resetpos()
+        if self.rect.bottom>800:
+            self.resetpos() 
 blocklist=pygame.sprite.Group()
 allspritelist=pygame.sprite.Group()
 for i in range(51):
@@ -27,12 +27,21 @@ for i in range(51):
     bb.rect.y=random.randint(0,800)
     blocklist.add(bb)
     allspritelist.add(bb)
+
+class p2(bob):
+    def update(self):
+        pos=pygame.mouse.get_pos()
+        self.rect.x=pos[0]
+        self.rect.y=pos[1]
+aa=p2("blue",15,20)
+allspritelist.add(aa)
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit()
     screen.fill("white")
     allspritelist.update()
+    blockhitlist=pygame.sprite.spritecollide(aa,blocklist,False)
     for i in blocklist:
       i.resetpos()
     allspritelist.draw(screen)
